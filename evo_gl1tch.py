e08982c740aabbb7e574935101c71a7ed1e5a87b
Christian Blum <christianb314@gmail.com>
2014-09-21 22:37:32 +0200

    added show_mod() and test() to genome class and genome is now self-testing on init

--- evo_gl1tch.py
+++ evo_gl1tch.py
@@ -46,12 +46,30 @@ class ActionAdd(Action):
 class Genome(object):
   def __init__(self, filename, length):
     self.im_data = list(open(filename, "rb").read())
-    self.genome = [ActionDoNothing(len(self.im_data)) for i in range(length)]
+    size = len(self.im_data)
+    self.genome = [random.choice([ActionDoNothing(size), ActionDelete(size), ActionAdd(size)]) for i in range(length)]
+    while not self.test():
+      self.genome = [random.choice([ActionDoNothing(size), ActionDelete(size), ActionAdd(size)]) for i in range(length)]
   def show_orig(self):
     Image.open(BytesIO("".join(self.im_data))).show()
+  def show_mod(self):
+    im = list(self.im_data)
+    for action in self.genome:
+      im = action.mod(im)
+    Image.open(BytesIO("".join(im))).show()
+  def test(self):
+    im = list(self.im_data)
+    for action in self.genome:
+      im = action.mod(im)
+    try:
+      Image.open(BytesIO("".join(im))).load()
+    except:
+      return False
+    return True
 
 if __name__ == "__main__":
   if len(sys.argv) != 2:
     print "USAGE: " + sys.argv[0] + " image"
     sys.exit()
   gen = Genome(sys.argv[1], 5)
+  gen.show_mod()
